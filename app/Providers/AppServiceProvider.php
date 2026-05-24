cb7fb53a946e7e3083ecdbfb5c2e07ef418ffaf4
k3ithz <h.baslan.554243@umindanao.edu.ph>
2026-05-24 21:25:00 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,12 +19,12 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void{
-    if (env('APP_ENV') === 'production') {
-URL::forceScheme('https');
-}
+    public function boot(): void
     {
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Paginator::useBootstrap();
     }
-}
 }
